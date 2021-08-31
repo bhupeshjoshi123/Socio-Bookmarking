682c14512b238628cc585d1126995cd5e00107e9
Bhupesh joshi <38806978+bhupeshjoshi123@users.noreply.github.com>
2021-08-31 14:17:43 +0545

    updated using enum

--- src/com/thrillio/constants/UserType.java
+++ src/com/thrillio/constants/UserType.java
@@ -1,11 +1,12 @@
 package com.thrillio.constants;
 
-public class UserType {
-    private UserType(){
-
-    }
-    public static final String USER = "user";
-    public static final String EDITOR = "editor";
-    public static final String CHIEF_EDITOR = "chiefeditor";
-
+public enum UserType {
+	// private cnstructor used so can not instantiate class.
+	USER("user"),
+	EDITOR("editor"),
+	CHIEF_EDITOR("chiefeditor");
+	String name;
+	private UserType(String name) {
+		this.name = name;
+	}
 }
