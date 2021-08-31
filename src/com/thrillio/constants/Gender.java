31e95523ec90cdca5072fa425eb0bad27aa74911
Bhupesh joshi <38806978+bhupeshjoshi123@users.noreply.github.com>
2021-08-31 13:48:52 +0545

    Update Gender.java

--- src/com/thrillio/constants/Gender.java
+++ src/com/thrillio/constants/Gender.java
@@ -1,11 +1,17 @@
 package com.thrillio.constants;
 
-public class Gender {
-    private Gender(){
-
-    }
-
-    public static final int MALE = 0;
-    public static final int FEMALE = 1;
-    public static final int TRANSGENDER = 2;
+public enum Gender {
+	MALE(0),
+	FEMALE(1),
+	TRANSGENDER(2);
+    
+	int index;
+	private Gender(int index) 
+	{
+		this.index = index;
+	}
+	
+	int getIndex() {
+		return index;
+	}
 }
