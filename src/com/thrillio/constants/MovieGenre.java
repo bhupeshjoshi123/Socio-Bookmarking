46407be19e6fc7e4d0eee109345935489b8979d9
Bhupesh joshi <38806978+bhupeshjoshi123@users.noreply.github.com>
2021-08-31 14:03:05 +0545

    updated with enum

--- src/com/thrillio/constants/MovieGenre.java
+++ src/com/thrillio/constants/MovieGenre.java
@@ -1,38 +1,34 @@
 package com.thrillio.constants;
 
-public class MovieGenre {
-    private MovieGenre() {
-    }
-    public static String CLASSICS = "Classics";
-    public static String DRAMA = "Drama";
-    public static String SIFY_AND_FANTASY = "Sci-Fi & Fantasy";
-    public static String CHILDREN_AND_FAMILY = "Children & Family";
-    public static String ACTION_AND_ADVENTURE = "Action & Adventure";
-    public static String THRILLERS = "Thrillers";
-    public static String MUSIC_AND_MUSICALS = "Music & Musicals";
-    public static String TELEVISION = "Television";
-    public static String HORROR = "Horrors";
-    public static String SPECIAL_INTREST = "Special Intrest";
-    public static String INDEPENDENT = "Independent";
-    public static String SPORTS_AND_FITNESS = "Sports & Fitness";
-    public static String ANIME_AND_ANIMATION = "Anime & Animation";
-    public static String GAY_AND_LESBIANS = "Gay & Lesbians";
-    public static String CLASSIC_MOVIE_MUSICALS = "Classic & Sprituality";
-    public static String FAITH_AND_SPRITUALITY = "Faith & Sprituality";
-    public static String FOREIGN_DRAMAS = "Foreign Dramas";
-    public static String FOREIGN_ACTION_AND_ADVENTURES = "Foreign action and Adventures";
-    public static String FOREIGN_THRILLERS = "Foreign Thrillers";
-    public static String TV_SHOWS = "Tv Show";
-    public static String DRAMAS = "Drama";
-    public static String ROMANTIC_MOVIES = "Romancic Movies";
-    public static String COMEDIES = "Comedies";
-    public static String DOCUMENTARIES = "Documentaries";
-    public static String FOREIGN_MOVIES = "Foreign_movies";
-
-
-
-
-
-
-
+public enum MovieGenre {
+    CLASSICS("Classics"),
+    DRAMA("Drama"),
+    SCIFI_AND_FANTASY("Sci-Fi & Fantasy"),
+    COMEDY("Comedy"),
+    CHILDREN_AND_FAMILY("Children & Family"),
+    ACTION_AND_ADVENTURE("Action & Adventure"),
+    THRILLERS("Thrillers"),
+    MUSIC_AND_MUSICALS("Music & Musicals"),
+    TELEVISION("Television"),
+    HORROR("Horror"),
+    SPECIAL_INTEREST("Special Interest"),
+    INDEPENDENT("Independent"),
+    SPORTS_AND_FITNESS("Sports & Fitness"),
+    ANIME_AND_ANIMATION("Anime & Animation"),
+    GAY_AND_LESBIAN("Gay & Lesbian"),
+    CLASSIC_MOVIE_MUSICALS("Classic Movie Musicals"),
+    FAITH_AND_SPIRITUALITY("Faith & Spirituality"),
+    FOREIGN_DRAMAS("Foreign Dramas"),
+    FOREGIN_ACTION_AND_ADVENTURE("Foreign Action & Adventure"),
+    FOREGIN_THRILLERS("Foreign Thrillers"),
+    TV_SHOWS("TV Shows"),
+    DRAMAS("Dramas"),
+    ROMANTIC_MOVIES("Romantic Movies"),
+    COMEDIES("Comedies"),
+    DOCUMENTARIES("Documentaries"),
+    FOREIGN_MOVIES("Foreign Movies");
+	String name;
+    private MovieGenre(String name) {
+		this.name = name;
+	}
 }
